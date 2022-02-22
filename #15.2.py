9d5e303a27602ebdf0b9dabc3c907a49748f0db6
Emil Tucharz <emil.tucharz@gmail.com>
2022-02-22 18:11:13 +0100

    Add 15.2.py file

---
+++ #15.2.py
@@ -0,0 +1,38 @@
+'''
+Write a program (using functions!) that asks the user for a long string containing multiple words.
+Print back to the user the same string, except with the words in backwards order.
+For example, say I type the string:
+
+  My name is Michele
+Then I would see the string:
+
+  Michele is name My
+shown back to me.
+'''
+
+# (3) Accessing Elements in Reversed Order
+
+# create function that ask for string using input and return given string
+
+
+def ask_for_string():
+    givenString = input(
+        "Please write some long string containing multiple words ")
+    return givenString
+
+# create function that takes given string as a parameter and return reversed string
+
+
+def reverse_given_string(givenString):
+    reversedString = ""
+    for word in reversed(givenString.split()):
+        reversedString += word + " "
+
+    return reversedString
+
+
+givenString = ask_for_string()
+
+reversedGivenString = reverse_given_string(givenString)
+
+print(reversedGivenString)
