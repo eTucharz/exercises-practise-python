a5ebc3fc9d116406e253d061839991a33c897af5
Emil Tucharz <emil.tucharz@gmail.com>
2022-02-27 19:00:24 +0100

    Add exercise description in comment

--- #19.py
+++ #19.py
@@ -1,3 +1,8 @@
+'''
+Using the requests and BeautifulSoup Python libraries, print to the screen the full text of the article on this website:
+http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture.
+'''
+
 import requests
 from bs4 import BeautifulSoup
 
