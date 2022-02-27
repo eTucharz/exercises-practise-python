630eb591442e1f809bd438efce74089c74be28a4
Emil Tucharz <emil.tucharz@gmail.com>
2022-02-27 14:33:31 +0100

    Add #19.py file

---
+++ #19.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture'
+r = requests.get(url)
+r_html = r.text
+
+soup = BeautifulSoup(r_html, features="html.parser")
+
+
+def get_article_paragraphs(soup):
+    for div in soup.find_all('div', class_='body__inner-container'):
+        for p in div.find_all('p'):
+            print(p.get_text().strip() + "\n")
+
+
+article_paragraphs = get_article_paragraphs(soup)
+
+print(article_paragraphs)
